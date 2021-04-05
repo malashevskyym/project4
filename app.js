670ca7bbb35e49b9aefd7eb9f6be43ae57caa306
malashevskyym <81178491+malashevskyym@users.noreply.github.com>
2021-04-04 22:25:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,62 @@ var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 
 var app = express();
+
+//Import the mongoose module
+var mongoose = require('mongoose');
+var dev_db_url = 'mongodb+srv://malashevskyym:Madrid19.@cluster0.gjowx.mongodb.net/Album?retryWrites=true&w=majority&ssl=true';
+//Set up default mongoose connection
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
+//var mongoDB = "mongodb://malashevskyym:Madrid19.@cluster0-shard-00-00.gjowx.mongodb.net:27017,cluster0-shard-00-01.gjowx.mongodb.net:27017,cluster0-shard-00-02.gjowx.mongodb.net:27017/Album?ssl=true&replicaSet=atlas-12ak39-shard-0&authSource=admin&retryWrites=true&w=majority"
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var catalogRouter = require('./routes/catalog');
+
+var app = express();
 //Set up mongoose connection
 var mongoose = require('mongoose');
 var mongoDB = 'insert_your_database_url_here';
