bb32365ac3ea9cba5965222d640e1e62b8a013a9
malashevskyym <81178491+malashevskyym@users.noreply.github.com>
2021-04-04 22:23:24 -0400

    Update albumController.js

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -1,6 +1,18 @@
+var Group = require('../models/group');
+var Comment = require('../models/comment');
 var Album = require('../models/album');
+
+var async = require('async');
 
 //Display list of Bands
 exports.album_list_id = function(req, res) {
-  res.send('NOT IMPLEMENTED: album list');
+  Album.find({'group': {$eq: req.param("id")}}, 'title year _id')
+     .populate('album')
+     .sort({'year': 1})
+     .exec(function(err, list_albums) {
+       if (err) {return next(err); }
+        //Successful, so render
+       res.json(list_albums);
+     });
+  //res.send('NOT IMPLEMENTED: album list');
 };
