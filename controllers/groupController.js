7a21e486179b5e5411b48100490b8e00b538dc7a
malashevskyym <81178491+malashevskyym@users.noreply.github.com>
2021-04-04 22:24:22 -0400

    Update groupController.js

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -6,8 +6,16 @@ var async = require('async');
 
 exports.index = function(req, res) {
     res.send('NOT IMPLEMENTED: Site Home Page');
-};
+  };
 //Display list of Bands
-exports.group_list = function(req, res) {
-  res.send('NOT IMPLEMENTED: Group list');
+exports.group_list = function(req, res, next) {
+  //res.send('Hello!');
+  Group.find({}, 'name bio')
+     .populate('album')
+     .sort({'name': 1})
+     .exec(function(err, list_groups) {
+       if (err) {return next(err); }
+        //Successful, so render
+       res.json(list_groups);
+     });
 };
