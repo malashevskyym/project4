d6027b613a8a27e039e6ddb31ef36f886014396d
malashevskyym <81178491+malashevskyym@users.noreply.github.com>
2021-04-07 21:05:11 -0400

    Update commentController.js

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -11,21 +11,43 @@ exports.comment_list = function(req, res) {
   Comment.find({}, 'text name email timedate')
      .populate('Comment')
      .sort('-timedate')
-     .limit(2)
-     //.limit(req.param({$lte: "nbr"}))
+     .limit(5)
      .exec(function(err, list_comments) {
        if (err) {return next(err); }
         //Successful, so render
        res.json(list_comments);
      });
-  //res.send('NOT IMPLEMENTED: Comments');
 };
 exports.comment_list_group = function(req, res) {
-  res.send('NOT IMPLEMENTED: Comments by group');
+  async.parallel({
+    group_id: function (callback) {
+      Album.find({'group': {$eq: req.param("id")}}, '_id')
+        .exec(callback)
+    },
+    group_comments: function(callback) {
+      Comment.find({'album': {group_id}}, 'text name email timedate')
+    },
+  }, function (err, results) {
+        if (err) { return next(err); } // Error in API usage.
+        if (results.album == null) { // No results.
+            var err = new Error('Group not found');
+            err.status = 404;
+            return next(err);
+        }
+    res.json(album_comments);
+  });
 };
 
 exports.comment_list_album = function(req, res) {
-  res.send('NOT IMPLEMENTED: Comments by album');
+  Comment.find({'album': {$eq: req.param("id")}}, 'text name email timedate album')
+     .populate('Comment')
+     .sort('-timedate')
+     .limit(5)
+     .exec(function(err, list_comments_album) {
+       if (err) {return next(err); }
+        //Successful, so render
+       res.json(list_comments_album);
+     });
 };
 
 //Display comment create form on GET.
